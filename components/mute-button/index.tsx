df0e4ba219309aff350f29d0c52b9eec7691e0b4
git-marcopitra <marcomorete73@hotmail.com>
2024-09-16 14:25:12 +0100

    :sparkles: feat: add mute and unmute

--- components/mute-button/index.tsx
+++ components/mute-button/index.tsx
@@ -1,5 +1,5 @@
 import { Motion } from '@interest-protocol/ui-kit';
-import { Button } from '@stylin.js/elements';
+import { Button, Div, P } from '@stylin.js/elements';
 import { type FC, useState } from 'react';
 
 import { MutedSVG, UnmutedSVG } from '../svg';
@@ -11,52 +11,63 @@ const MuteButton: FC<MuteButtonProps> = ({ videoRef }) => {
 
   const toggleMute = () => {
     if (videoRef.current) {
-      if (firstTime) setFirstTime(false);
       setMuted(!videoRef.current.muted);
       videoRef.current.muted = !videoRef.current.muted;
+      if (firstTime) setFirstTime(false);
     }
   };
 
   return (
-    <Motion
+    <Div
+      gap="1rem"
       right="2rem"
       bottom="2rem"
+      display="flex"
       position="fixed"
-      initial={{ scale: 1, x: 0 }}
-      animate={{
-        scale: muted ? [1, 1.5, 1] : [1],
-        x:
-          firstTime && muted
-            ? [
-                1, -1, 1, -1, 1, 1, -1, 1, -1, 1, -2, 2, -2, 2, -3, 3, -3, 3,
-                -3, 3, -2, 2, 2, -2, 2, -2, 1, -1, 1, -1, 1, -1, 1, -1,
-              ]
-            : [0],
-      }}
-      transition={{ duration: 3, repeat: Infinity }}
+      alignItems="center"
+      onClick={toggleMute}
     >
-      <Button
-        all="unset"
-        width="3rem"
-        height="3rem"
-        color="white"
-        display="flex"
-        cursor="pointer"
-        borderRadius="50%"
-        alignItems="center"
-        onClick={toggleMute}
-        justifyContent="center"
-        backdropFilter="blur(5px)"
-        boxShadow="0px 1.2px 29.92px 0px #452A7C1A"
-        bg="linear-gradient(86.16deg, rgba(255, 255, 255, 0.2) 11.14%, rgba(255, 255, 255, 0.035) 113.29%)"
+      {firstTime && (
+        <P px="1rem" py="0.75rem" bg="#fff" borderRadius="0.5rem">
+          Sound ON
+        </P>
+      )}
+      <Motion
+        initial={{ scale: 1, x: 0 }}
+        transition={{ duration: 3, repeat: Infinity }}
+        animate={{
+          scale: muted ? [1, 1.2, 1] : [1],
+          x:
+            firstTime && muted
+              ? [
+                  1, -1, 1, -1, 1, 1, -1, 1, -1, 1, -2, 2, -2, 2, -3, 3, -3, 3,
+                  -3, 3, -2, 2, 2, -2, 2, -2, 1, -1, 1, -1, 1, -1, 1, -1,
+                ]
+              : [0],
+        }}
       >
-        {muted ? (
-          <MutedSVG maxHeight="1rem" maxWidth="1rem" height="100%" />
-        ) : (
-          <UnmutedSVG maxHeight="1rem" maxWidth="1rem" height="100%" />
-        )}
-      </Button>
-    </Motion>
+        <Button
+          all="unset"
+          width="3rem"
+          height="3rem"
+          color="white"
+          display="flex"
+          cursor="pointer"
+          borderRadius="50%"
+          alignItems="center"
+          justifyContent="center"
+          backdropFilter="blur(5px)"
+          boxShadow="0px 1.2px 29.92px 0px #452A7C1A"
+          bg="linear-gradient(86.16deg, rgba(255, 255, 255, 0.2) 11.14%, rgba(255, 255, 255, 0.035) 113.29%)"
+        >
+          {muted ? (
+            <MutedSVG maxHeight="1rem" maxWidth="1rem" height="100%" />
+          ) : (
+            <UnmutedSVG maxHeight="1rem" maxWidth="1rem" height="100%" />
+          )}
+        </Button>
+      </Motion>
+    </Div>
   );
 };
 
