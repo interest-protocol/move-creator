91d456e4e766205a0ec270dd6154972f388aae7a
git-marcopitra <marcomorete73@hotmail.com>
2024-09-19 03:03:15 +0100

    :sparkles: feat: responsive close button

--- components/buy-now/index.tsx
+++ components/buy-now/index.tsx
@@ -1,6 +1,6 @@
 import { Modal, Motion } from '@interest-protocol/ui-kit';
 import { SUI_TYPE_ARG } from '@mysten/sui/utils';
-import { Button, ButtonProps } from '@stylin.js/elements';
+import { Button, ButtonProps, Div } from '@stylin.js/elements';
 import dynamic from 'next/dynamic';
 import { FC, useState } from 'react';
 
@@ -32,29 +32,29 @@ const BuyNow: FC<ButtonProps> = (props) => {
         Buy now
       </Button>
       <Modal custom isOpen={open} onClose={() => setOpen(false)}>
-        <Button
-          all="unset"
-          top="1rem"
-          right="1rem"
-          bg="#9FECFE"
-          width="3rem"
-          height="3rem"
-          cursor="pointer"
-          fontWeight="600"
-          textAlign="center"
-          borderRadius="50%"
-          position="absolute"
-          onClick={() => setOpen(false)}
-        >
-          <TimesSVG maxWidth="0.85rem" maxHeight="0.85rem" width="100%" />
-        </Button>
-        <Motion
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
-        >
-          <SwapInterface fixedOut typeIn={SUI_TYPE_ARG} typeOut={CA} />
-        </Motion>
+        <Div display="flex" flexDirection="column">
+          <Button
+            all="unset"
+            ml="auto"
+            mb="1rem"
+            bg="#9FECFE"
+            cursor="pointer"
+            textAlign="center"
+            borderRadius="50%"
+            onClick={() => setOpen(false)}
+            width={['2rem', '2rem', '3rem']}
+            height={['2rem', '2rem', '3rem']}
+          >
+            <TimesSVG maxWidth="0.85rem" maxHeight="0.85rem" width="100%" />
+          </Button>
+          <Motion
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}
+          >
+            <SwapInterface fixedOut typeIn={SUI_TYPE_ARG} typeOut={CA} />
+          </Motion>
+        </Div>
       </Modal>
     </>
   );
